58e3eeefbe8d7bd66547223ebdf7e542ff47baf2
Idevaldo Neto <inetomec@yahoo.com.br>
2022-10-18 23:26:25 -0300

    refactor: Criados os métodos findAll e findById em UserService e implementados em UserServiceImpl

--- authuser/src/main/java/com/ead/authuser/services/impl/UserServiceImpl.java
+++ authuser/src/main/java/com/ead/authuser/services/impl/UserServiceImpl.java
@@ -1,13 +1,32 @@
 package com.ead.authuser.services.impl;
 
+import com.ead.authuser.models.UserModel;
 import com.ead.authuser.repositories.UserRepository;
 import com.ead.authuser.services.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Optional;
+import java.util.UUID;
 
 @Service
 public class UserServiceImpl implements UserService {
 
     @Autowired
     UserRepository userRepository;
+
+    @Override
+    public List<UserModel> findAll() {
+        List<UserModel> users = this.userRepository.findAll();
+
+        return users;
+    }
+
+    @Override
+    public Optional<UserModel> findById(UUID id) {
+        Optional<UserModel> userModelOptional = this.userRepository.findById(id);
+
+        return userModelOptional;
+    }
 }
