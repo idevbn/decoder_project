dba15e3f806d16c172dfeb6a53a1d9d86e0a7e17
Idevaldo Neto <inetomec@yahoo.com.br>
2022-11-02 04:28:01 -0300

    refactor: Adicionadas validações nos campos do UserDTO

--- authuser/src/main/java/com/ead/authuser/controllers/dtos/UserDTO.java
+++ authuser/src/main/java/com/ead/authuser/controllers/dtos/UserDTO.java
@@ -5,6 +5,9 @@ import com.fasterxml.jackson.annotation.JsonInclude;
 import com.fasterxml.jackson.annotation.JsonView;
 import lombok.Data;
 
+import javax.validation.constraints.Email;
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.Size;
 import java.util.UUID;
 
 /**
@@ -25,16 +28,27 @@ public class UserDTO {
 
     private UUID id;
 
+    /**
+     * O @NotBlank não permite valores NULOS nem valores VAZIOS
+     */
+    @Size(min = 4, max = 50)
     @JsonView(UserView.RegistrationPost.class)
+    @NotBlank(groups = UserView.RegistrationPost.class)
     private String username;
 
+    @Email
     @JsonView(UserView.RegistrationPost.class)
+    @NotBlank(groups = UserView.RegistrationPost.class)
     private String email;
 
+    @Size(min = 6, max = 20)
     @JsonView({UserView.RegistrationPost.class, UserView.PasswordPut.class})
+    @NotBlank(groups = {UserView.RegistrationPost.class, UserView.PasswordPut.class})
     private String password;
 
+    @Size(min = 6, max = 20)
     @JsonView(UserView.PasswordPut.class)
+    @NotBlank(groups = UserView.PasswordPut.class)
     private String oldPassword;
 
     @JsonView({UserView.RegistrationPost.class, UserView.UserPut.class})
@@ -47,6 +61,7 @@ public class UserDTO {
     private String cpf;
 
     @JsonView(UserView.ImagePut.class)
+    @NotBlank(groups = UserView.ImagePut.class)
     private String imageUrl;
 
 }
