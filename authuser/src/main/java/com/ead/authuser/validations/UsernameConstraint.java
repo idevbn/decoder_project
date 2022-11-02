67e83e2019a2e9c5c366afac45a40961decaa243
Idevaldo Neto <inetomec@yahoo.com.br>
2022-11-02 12:03:18 -0300

    feat: Criada a validação customizada @UsernameConstraint, que valida o campo username

---
+++ authuser/src/main/java/com/ead/authuser/validations/UsernameConstraint.java
@@ -0,0 +1,17 @@
+package com.ead.authuser.validations;
+
+import javax.validation.Constraint;
+import javax.validation.Payload;
+import java.lang.annotation.*;
+
+@Documented
+@Retention(RetentionPolicy.RUNTIME)
+@Target({ElementType.METHOD, ElementType.FIELD})
+@Constraint(validatedBy = UsernameConstraintImpl.class)
+public @interface UsernameConstraint {
+
+    String message() default "Invalid username";
+    Class<?>[] groups() default {};
+    Class<? extends Payload>[] payload() default{};
+
+}
