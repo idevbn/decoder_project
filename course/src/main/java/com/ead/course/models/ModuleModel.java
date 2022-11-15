0b7ccc74c54299e8fc1e4adc46635c34353d3087
Idevaldo Neto <inetomec@yahoo.com.br>
2022-11-15 10:59:07 -0300

    feat: Criados os models CourseModel, LessonModel e ModuleModel

---
+++ course/src/main/java/com/ead/course/models/ModuleModel.java
@@ -0,0 +1,34 @@
+package com.ead.course.models;
+
+import com.fasterxml.jackson.annotation.JsonFormat;
+import com.fasterxml.jackson.annotation.JsonInclude;
+import lombok.Data;
+
+import javax.persistence.*;
+import java.io.Serializable;
+import java.time.LocalDateTime;
+import java.util.UUID;
+
+@Data
+@Entity
+@Table(name = "tb_modules")
+@JsonInclude(JsonInclude.Include.NON_NULL)
+public class ModuleModel implements Serializable {
+
+    private static final long serialVersionUID = 1L;
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private UUID id;
+
+    @Column(nullable = false, unique = true, length = 150)
+    private String title;
+
+    @Column(nullable = false, unique = true, length = 250)
+    private String description;
+
+    @Column(nullable = false)
+    @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "dd-MM-yyyy HH:mm:ss")
+    private LocalDateTime creationDate;
+
+}
