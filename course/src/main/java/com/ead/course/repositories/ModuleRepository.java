66ae6c5b2ce6512f8988ad0a58c341084fdeffa9
Idevaldo Neto <inetomec@yahoo.com.br>
2022-11-15 13:10:18 -0300

    refactor: Adicionada consulta customizada utilizando o @Query em ModuleRepository

--- course/src/main/java/com/ead/course/repositories/ModuleRepository.java
+++ course/src/main/java/com/ead/course/repositories/ModuleRepository.java
@@ -2,8 +2,27 @@ package com.ead.course.repositories;
 
 import com.ead.course.models.ModuleModel;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
+import java.util.List;
 import java.util.UUID;
 
 public interface ModuleRepository extends JpaRepository<ModuleModel, UUID> {
+
+    /**
+     * O @Query é utilizado apenas para CONSULTAS.
+     *
+     * Para queries de atualização, remoção e criação
+     * é preciso utilizar além do @Query, o @Modifying.
+     *
+     * Consulta que busca todos os elementos da tabela
+     * tb_modules para o caso no qual a chave estrangeira
+     * course_id é igual ao
+     * @param id
+     * @return
+     */
+    @Query(value = "SELECT * FROM tb_modules WHERE course_id = :id", nativeQuery = true)
+    List<ModuleModel> findAllModulesIntoCourse(@Param("id") final UUID id);
+
 }
