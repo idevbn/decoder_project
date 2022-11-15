e24180f8f5b49eaf306313c4b39ff7876ba9abfc
Idevaldo Neto <inetomec@yahoo.com.br>
2022-11-15 14:12:46 -0300

    refactor: Criado e implementado o método delete para realizar remoção em cascata a partir do ModuleModel

--- course/src/main/java/com/ead/course/services/impl/ModuleServiceImpl.java
+++ course/src/main/java/com/ead/course/services/impl/ModuleServiceImpl.java
@@ -1,18 +1,42 @@
 package com.ead.course.services.impl;
 
+import com.ead.course.models.LessonModel;
+import com.ead.course.models.ModuleModel;
+import com.ead.course.repositories.LessonRepository;
 import com.ead.course.repositories.ModuleRepository;
 import com.ead.course.services.ModuleService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import javax.transaction.Transactional;
+import java.util.List;
+
 @Service
 public class ModuleServiceImpl implements ModuleService {
 
     private final ModuleRepository moduleRepository;
+    private final LessonRepository lessonRepository;
 
     @Autowired
-    public ModuleServiceImpl(final ModuleRepository moduleRepository) {
+    public ModuleServiceImpl(
+            final ModuleRepository moduleRepository,
+            final LessonRepository lessonRepository
+    ) {
         this.moduleRepository = moduleRepository;
+        this.lessonRepository = lessonRepository;
+    }
+
+    @Override
+    @Transactional
+    public void delete(final ModuleModel moduleModel) {
+        final List<LessonModel> lessonModelList
+                = this.lessonRepository.findAllLessonsIntoModule(moduleModel.getId());
+
+        if (!lessonModelList.isEmpty()) {
+            this.lessonRepository.deleteAll(lessonModelList);
+        }
+
+        this.moduleRepository.delete(moduleModel);
     }
 
 }
