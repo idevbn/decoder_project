4aa7cf035427dcc7c82600e01ccfef66d7d87efa
Idevaldo Neto <inetomec@yahoo.com.br>
2022-12-18 20:14:06 -0300

    feat: Criada entidade CourseUserModel para relacionar um curso com um usuário no microsserviço de cursos

---
+++ course/src/main/java/com/ead/course/models/CourseUserModel.java
@@ -0,0 +1,28 @@
+package com.ead.course.models;
+
+import com.fasterxml.jackson.annotation.JsonInclude;
+import lombok.Data;
+
+import javax.persistence.*;
+import java.io.Serializable;
+import java.util.UUID;
+
+@Data
+@Entity
+@Table(name = "tb_courses_users")
+@JsonInclude(JsonInclude.Include.NON_NULL)
+public class CourseUserModel implements Serializable {
+
+    private static final long serialVersionUID = 1L;
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private UUID id;
+
+    @ManyToOne(fetch = FetchType.LAZY, optional = false)
+    private CourseModel course;
+
+    @Column(nullable = false)
+    private UUID userId;
+
+}
