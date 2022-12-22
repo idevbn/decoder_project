e1e507ef2607d6a3cd5e4b085aa5c89b103c2b0c
Idevaldo Neto <inetomec@yahoo.com.br>
2022-12-22 00:01:45 -0300

    style: Adicionada linha em branco

--- authuser/src/main/java/com/ead/authuser/models/UserCourseModel.java
+++ authuser/src/main/java/com/ead/authuser/models/UserCourseModel.java
@@ -14,6 +14,7 @@ import java.util.UUID;
 public class UserCourseModel implements Serializable {
 
     private static final long serialVersionUID = 1L;
+
     @Id
     @GeneratedValue(strategy = GenerationType.AUTO)
     private UUID id;
