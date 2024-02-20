1a1b7f81405dcdd9759813e47d8f65146a3b7aed
Idevaldo Neto <inetomec@yahoo.com.br>
2024-02-19 21:45:21 -0300

    feat: Criado endpoint responsável pela inscrição de um usuário em um curso

--- course/src/main/java/com/ead/course/controllers/CourseUserController.java
+++ course/src/main/java/com/ead/course/controllers/CourseUserController.java
@@ -1,7 +1,12 @@
 package com.ead.course.controllers;
 
 import com.ead.course.clients.CourseClient;
+import com.ead.course.dtos.SubscriptionDTO;
 import com.ead.course.dtos.UserDTO;
+import com.ead.course.models.CourseModel;
+import com.ead.course.models.CourseUserModel;
+import com.ead.course.services.CourseService;
+import com.ead.course.services.CourseUserService;
 import lombok.extern.log4j.Log4j2;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
@@ -10,11 +15,10 @@ import org.springframework.data.domain.Sort;
 import org.springframework.data.web.PageableDefault;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.CrossOrigin;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
+import javax.validation.Valid;
+import java.util.Optional;
 import java.util.UUID;
 
 @Log4j2
@@ -23,10 +27,16 @@ import java.util.UUID;
 public class CourseUserController {
 
     private final CourseClient courseClient;
+    private final CourseService courseService;
+    private final CourseUserService courseUserService;
 
     @Autowired
-    public CourseUserController(final CourseClient courseClient) {
+    public CourseUserController(final CourseClient courseClient,
+                                final CourseService courseService,
+                                final CourseUserService courseUserService) {
         this.courseClient = courseClient;
+        this.courseService = courseService;
+        this.courseUserService = courseUserService;
     }
 
     @GetMapping(value = "/courses/{courseId}/users")
@@ -43,6 +53,32 @@ public class CourseUserController {
                 .body(allCoursesByUser);
 
         return usersByCourseResponse;
+    }
+
+    @PostMapping(value = "/courses/{courseId}/users/subscription")
+    public ResponseEntity<?> saveSubscriptionUserInCourse(
+            @PathVariable(value = "courseId") final UUID courseId,
+            @RequestBody @Valid final SubscriptionDTO subscriptionDTO
+    ) {
+        final Optional<CourseModel> optionalCourseModel = this.courseService.findById(courseId);
+
+        if (optionalCourseModel.isEmpty()) {
+            return ResponseEntity.status(HttpStatus.NOT_FOUND).build();
+        }
+
+        final boolean existsByCourseAndUserId = this.courseUserService
+                .existsByCourseAndUserId(optionalCourseModel.get(), subscriptionDTO.getUserId());
+
+        if (existsByCourseAndUserId) {
+            return ResponseEntity.status(HttpStatus.CONFLICT).build();
+        }
+
+        // Necessário fazer verificação de user
+
+        final CourseUserModel courseUserModel = this.courseUserService
+                .save(optionalCourseModel.get().convertToCourseUserModel(subscriptionDTO.getUserId()));
+
+        return ResponseEntity.status(HttpStatus.CREATED).build();
     }
 
 }
