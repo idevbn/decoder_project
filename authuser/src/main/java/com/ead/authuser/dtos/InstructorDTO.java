03c7336dad40905f971754757757189d201ae904
Idevaldo Neto <inetomec@yahoo.com.br>
2024-02-20 15:57:37 -0300

    feat: Criado DTO para envio de dados de um usuário instrutor de um curso

---
+++ authuser/src/main/java/com/ead/authuser/dtos/InstructorDTO.java
@@ -0,0 +1,14 @@
+package com.ead.authuser.dtos;
+
+import lombok.Getter;
+import lombok.Setter;
+
+import java.util.UUID;
+
+@Getter
+@Setter
+public class InstructorDTO {
+
+    private UUID userId;
+
+}
