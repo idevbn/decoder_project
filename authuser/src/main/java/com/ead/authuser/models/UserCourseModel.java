32ff3b64b1b6b71133f339d41995f0393e2e5bb2
Idevaldo Neto <inetomec@yahoo.com.br>
2024-02-20 01:37:05 -0300

    feat: Adicionadas annotations para permitir a instanciação dos objetos com todos os parâmetros ou sem nenhum

--- authuser/src/main/java/com/ead/authuser/models/UserCourseModel.java
+++ authuser/src/main/java/com/ead/authuser/models/UserCourseModel.java
@@ -1,7 +1,9 @@
 package com.ead.authuser.models;
 
 import com.fasterxml.jackson.annotation.JsonInclude;
+import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 import javax.persistence.*;
 import java.io.Serializable;
@@ -9,6 +11,8 @@ import java.util.UUID;
 
 @Data
 @Entity
+@NoArgsConstructor
+@AllArgsConstructor
 @Table(name = "tb_users_courses")
 @JsonInclude(JsonInclude.Include.NON_NULL)
 public class UserCourseModel implements Serializable {
