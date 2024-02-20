03230063f573e826aca9520d8057f122e0c4800a
Idevaldo Neto <inetomec@yahoo.com.br>
2024-02-20 01:03:09 -0300

    refactor: Alterada criação de url para requisição ao microsserviço course
    A raiz da requisição é obtida do application.yaml

--- authuser/src/main/java/com/ead/authuser/services/impl/UtilsServiceImpl.java
+++ authuser/src/main/java/com/ead/authuser/services/impl/UtilsServiceImpl.java
@@ -9,12 +9,9 @@ import java.util.UUID;
 @Service
 public class UtilsServiceImpl implements UtilsService {
 
-    public static final String REQUEST_URI = "http://localhost:8082";
-
     @Override
     public String createUrl(final UUID userId, final Pageable pageable) {
-        final String url = REQUEST_URI
-                + "/courses?userId=" + userId
+        final String url = "/courses?userId=" + userId
                 + "&page=" + pageable.getPageNumber()
                 + "&size=" + pageable.getPageSize()
                 + "&sort=" + pageable.getSort().toString().replaceAll(": ", ",");
