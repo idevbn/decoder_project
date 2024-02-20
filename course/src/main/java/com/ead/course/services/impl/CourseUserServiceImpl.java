35090cfd0d09f883ed63c0a6efcedcb539b39712
Idevaldo Neto <inetomec@yahoo.com.br>
2024-02-19 21:41:18 -0300

    feat: Criado e implementados métodos que verificam existência do curso e do usuário
    Também foi criado e implementado o método que persiste um curso

--- course/src/main/java/com/ead/course/services/impl/CourseUserServiceImpl.java
+++ course/src/main/java/com/ead/course/services/impl/CourseUserServiceImpl.java
@@ -1,9 +1,13 @@
 package com.ead.course.services.impl;
 
+import com.ead.course.models.CourseModel;
+import com.ead.course.models.CourseUserModel;
 import com.ead.course.repositories.CourseUserRepository;
 import com.ead.course.services.CourseUserService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import java.util.UUID;
 
 @Service
 public class CourseUserServiceImpl implements CourseUserService {
@@ -13,6 +17,16 @@ public class CourseUserServiceImpl implements CourseUserService {
     @Autowired
     public CourseUserServiceImpl(final CourseUserRepository courseUserRepository) {
         this.courseUserRepository = courseUserRepository;
+    }
+
+    @Override
+    public boolean existsByCourseAndUserId(final CourseModel courseModel, final UUID userId) {
+        return this.courseUserRepository.existsByCourseAndUserId(courseModel, userId);
+    }
+
+    @Override
+    public CourseUserModel save(final CourseUserModel courseUserModel) {
+        return this.courseUserRepository.save(courseUserModel);
     }
 
 }
