e787fe84c053ca9c0e94fb7d9578e6a95cbf55a0
Idevaldo Neto <inetomec@yahoo.com.br>
2024-03-07 21:15:28 -0300

    chore: Criado método que gera exchange fanout

--- authuser/src/main/java/com/ead/authuser/configs/RabbitmqConfig.java
+++ authuser/src/main/java/com/ead/authuser/configs/RabbitmqConfig.java
@@ -2,15 +2,20 @@ package com.ead.authuser.configs;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule;
+import org.springframework.amqp.core.FanoutExchange;
 import org.springframework.amqp.rabbit.connection.CachingConnectionFactory;
 import org.springframework.amqp.rabbit.core.RabbitTemplate;
 import org.springframework.amqp.support.converter.Jackson2JsonMessageConverter;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
 public class RabbitmqConfig {
+
+    @Value("${ead.broker.exchange.userEvent}")
+    private String exchangeUserEvent;
 
     private final CachingConnectionFactory cachingConnectionFactory;
 
@@ -31,6 +36,11 @@ public class RabbitmqConfig {
         final ObjectMapper objectMapper = new ObjectMapper();
         objectMapper.registerModule(new JavaTimeModule());
         return new Jackson2JsonMessageConverter(objectMapper);
+    }
+
+    @Bean
+    public FanoutExchange fanoutUserEvent() {
+        return new FanoutExchange(this.exchangeUserEvent);
     }
 
 }
