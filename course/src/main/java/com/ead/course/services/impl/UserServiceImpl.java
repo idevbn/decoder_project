7227850235a86a701356f98def5e54ad1fda2969
Idevaldo Neto <inetomec@yahoo.com.br>
2024-03-11 14:11:55 -0300

    feat: Adicionados métodos para remoção em cascata de CourseUser

--- course/src/main/java/com/ead/course/services/impl/UserServiceImpl.java
+++ course/src/main/java/com/ead/course/services/impl/UserServiceImpl.java
@@ -1,6 +1,7 @@
 package com.ead.course.services.impl;
 
 import com.ead.course.models.UserModel;
+import com.ead.course.repositories.CourseRepository;
 import com.ead.course.repositories.UserRepository;
 import com.ead.course.services.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -9,6 +10,7 @@ import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.domain.Specification;
 import org.springframework.stereotype.Service;
 
+import javax.transaction.Transactional;
 import java.util.Optional;
 import java.util.UUID;
 
@@ -16,10 +18,13 @@ import java.util.UUID;
 public class UserServiceImpl implements UserService {
 
     private final UserRepository userRepository;
+    private final CourseRepository courseRepository;
 
     @Autowired
-    public UserServiceImpl(final UserRepository userRepository) {
+    public UserServiceImpl(final UserRepository userRepository,
+                           final CourseRepository courseRepository) {
         this.userRepository = userRepository;
+        this.courseRepository = courseRepository;
     }
 
     @Override
@@ -33,7 +38,10 @@ public class UserServiceImpl implements UserService {
     }
 
     @Override
+    @Transactional
     public void delete(final UUID userId) {
+        this.courseRepository.deleteCourseUserByUser(userId);
+
         this.userRepository.deleteById(userId);
     }
 
