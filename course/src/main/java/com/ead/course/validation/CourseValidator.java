5ea85a1b883dc4c205a56a7bfd04a7a5d0ae2cf4
Idevaldo Neto <inetomec@yahoo.com.br>
2024-03-11 13:32:12 -0300

    feat: Implementada validação para um instrutor

--- course/src/main/java/com/ead/course/validation/CourseValidator.java
+++ course/src/main/java/com/ead/course/validation/CourseValidator.java
@@ -1,21 +1,27 @@
 package com.ead.course.validation;
 
 import com.ead.course.dtos.CourseDTO;
+import com.ead.course.enums.UserType;
+import com.ead.course.models.UserModel;
+import com.ead.course.services.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 import org.springframework.validation.Errors;
 import org.springframework.validation.Validator;
 
+import java.util.Optional;
 import java.util.UUID;
 
 @Component
 public class CourseValidator implements Validator {
 
     private final Validator validator;
+    private final UserService userService;
 
     @Autowired
-    public CourseValidator(final Validator validator) {
+    public CourseValidator(final Validator validator, final UserService userService) {
         this.validator = validator;
+        this.userService = userService;
     }
 
     @Override
@@ -34,28 +40,23 @@ public class CourseValidator implements Validator {
      }
 
      private void validateUserInstructor(final UUID userInstructor, final Errors errors) {
-//         ResponseEntity<UserDTO> responseUserInstructor;
+        final Optional<UserModel> userModelOpt = this.userService.findById(userInstructor);
 
-//         try {
-//             responseUserInstructor = this.authUserClient.getOneUserById(userInstructor);
-//
-//             if (responseUserInstructor.getBody().getUserType().equals(UserType.STUDENT)) {
-//                 errors.rejectValue(
-//                         "userInstructor",
-//                         "UserInstructorError",
-//                         "User must be INSTRUCTOR or ADMIN."
-//                 );
-//             }
-//
-//         } catch (final HttpStatusCodeException e) {
-//             if (e.getStatusCode().equals(HttpStatus.NOT_FOUND)) {
-//                 errors.rejectValue(
-//                         "userInstructor",
-//                         "UserInstructorError",
-//                         "Instructor not found."
-//                 );
-//             }
-//         }
+        if (userModelOpt.isEmpty()) {
+            errors.rejectValue(
+                         "userInstructor",
+                         "UserInstructorError",
+                         "Instructor not found."
+                 );
+        }
+
+        if (userModelOpt.get().getUserType().equals(UserType.STUDENT.toString())) {
+            errors.rejectValue(
+                         "userInstructor",
+                         "UserInstructorError",
+                         "User must be INSTRUCTOR or ADMIN."
+                 );
+        }
      }
 
 }
