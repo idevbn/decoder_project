d334d8025e176b13ce41c38a84b181a1eda1ee04
Idevaldo Neto <inetomec@yahoo.com.br>
2024-03-13 17:52:38 -0300

    feat: Criados e implementados métodos na camada de service

--- notification/src/main/java/com/ead/notification/services/impl/NotificationImpl.java
+++ notification/src/main/java/com/ead/notification/services/impl/NotificationImpl.java
@@ -1,10 +1,16 @@
 package com.ead.notification.services.impl;
 
+import com.ead.notification.enums.NotificationStatus;
 import com.ead.notification.models.NotificationModel;
 import com.ead.notification.repositories.NotificationRepository;
 import com.ead.notification.services.NotificationService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
+
+import java.util.Optional;
+import java.util.UUID;
 
 @Service
 public class NotificationImpl implements NotificationService {
@@ -31,6 +37,25 @@ public class NotificationImpl implements NotificationService {
     @Override
     public NotificationModel saveNotification(final NotificationModel notificationModel) {
         return this.notificationRepository.save(notificationModel);
+    }
+
+    @Override
+    public Page<NotificationModel> findAllNotificationsByUser(final UUID userId,
+                                                              final Pageable pageable) {
+        return this.notificationRepository
+                .findAllByUserIdAndNotificationStatus(
+                userId,
+                NotificationStatus.CREATED,
+                pageable
+        );
+    }
+
+    @Override
+    public Optional<NotificationModel> findByNotificationIdAndUserId(
+            final UUID notificationId,
+            final UUID userId
+    ) {
+        return this.notificationRepository.findByNotificationIdAndUserId(notificationId, userId);
     }
 
 }
