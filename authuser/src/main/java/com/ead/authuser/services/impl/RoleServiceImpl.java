8213a2ca164f524c23db79c8432f13bde40a4f55
Idevaldo Neto <inetomec@yahoo.com.br>
2024-03-15 20:11:30 -0300

    feat: Criado e implementado método no service que busca uma RoleModel pelo nome

--- authuser/src/main/java/com/ead/authuser/services/impl/RoleServiceImpl.java
+++ authuser/src/main/java/com/ead/authuser/services/impl/RoleServiceImpl.java
@@ -1,15 +1,25 @@
 package com.ead.authuser.services.impl;
 
+import com.ead.authuser.enums.RoleType;
+import com.ead.authuser.models.RoleModel;
 import com.ead.authuser.repositories.RoleRepository;
+import com.ead.authuser.services.RoleService;
 import org.springframework.stereotype.Service;
 
+import java.util.Optional;
+
 @Service
-public class RoleServiceImpl {
+public class RoleServiceImpl implements RoleService {
 
     private final RoleRepository roleRepository;
 
     public RoleServiceImpl(final RoleRepository roleRepository) {
         this.roleRepository = roleRepository;
+    }
+
+    @Override
+    public Optional<RoleModel> findByRoleName(final RoleType roleType) {
+        return this.roleRepository.findByRoleName(roleType);
     }
 
 }
