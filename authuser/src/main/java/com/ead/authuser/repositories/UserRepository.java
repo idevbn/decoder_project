ac00f26d6516b7c0780a5482e2dfab9ba9dca673
Idevaldo Neto <inetomec@yahoo.com.br>
2024-03-16 11:25:35 -0300

    feat: Criado método que recupera um usuário pelo username

--- authuser/src/main/java/com/ead/authuser/repositories/UserRepository.java
+++ authuser/src/main/java/com/ead/authuser/repositories/UserRepository.java
@@ -1,14 +1,22 @@
 package com.ead.authuser.repositories;
 
 import com.ead.authuser.models.UserModel;
+import org.springframework.data.jpa.repository.EntityGraph;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
 
+import java.util.Optional;
 import java.util.UUID;
 
 public interface UserRepository extends JpaRepository<UserModel, UUID>, JpaSpecificationExecutor<UserModel> {
 
     boolean existsByUsername(final String username);
     boolean existsByEmail(final String email);
+
+    /**
+     * Recupera as roles do usuário, mesmo elas definidas com FetchType.LAZY na entidade
+     */
+    @EntityGraph(attributePaths = "roles", type = EntityGraph.EntityGraphType.FETCH)
+    Optional<UserModel> findByUsername(final String username);
 
 }
