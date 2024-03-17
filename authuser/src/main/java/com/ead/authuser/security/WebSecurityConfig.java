16a00fada882f09cea9154c2281dd820333bd490
Idevaldo Neto <inetomec@yahoo.com.br>
2024-03-17 18:19:35 -0300

    chore: Criado bean que retorna um AuthenticationManager

--- authuser/src/main/java/com/ead/authuser/security/WebSecurityConfig.java
+++ authuser/src/main/java/com/ead/authuser/security/WebSecurityConfig.java
@@ -3,6 +3,7 @@ package com.ead.authuser.security;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpMethod;
+import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
@@ -48,6 +49,12 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
         auth
                 .userDetailsService(this.userDetailsService)
                 .passwordEncoder(this.passwordEncoder());
+    }
+
+    @Bean
+    @Override
+    public AuthenticationManager authenticationManagerBean() throws Exception {
+        return super.authenticationManagerBean();
     }
 
     @Bean
