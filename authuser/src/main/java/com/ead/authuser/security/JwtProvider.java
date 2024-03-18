a7d8894972ed304b4e3302f6a5b255a368c10454
Idevaldo Neto <inetomec@yahoo.com.br>
2024-03-18 08:50:28 -0300

    chore: Criados métodos getUsernameJwt e validateJwt

--- authuser/src/main/java/com/ead/authuser/security/JwtProvider.java
+++ authuser/src/main/java/com/ead/authuser/security/JwtProvider.java
@@ -1,7 +1,6 @@
 package com.ead.authuser.security;
 
-import io.jsonwebtoken.Jwts;
-import io.jsonwebtoken.SignatureAlgorithm;
+import io.jsonwebtoken.*;
 import lombok.extern.log4j.Log4j2;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.security.core.Authentication;
@@ -33,6 +32,34 @@ public class JwtProvider {
                 .setExpiration(Date.from(issuedAt.plus(Long.parseLong(this.jwtExpirationMs), ChronoUnit.MILLIS)))
                 .signWith(SignatureAlgorithm.HS512, this.jwtSecret)
                 .compact();
+    }
+
+    public String getUsernameJwt(final String token) {
+        return Jwts.parser()
+                .setSigningKey(this.jwtSecret)
+                .parseClaimsJws(token)
+                .getBody()
+                .getSubject();
+    }
+
+    public boolean validateJwt(final String authToken) {
+        try {
+            Jwts.parser().setSigningKey(this.jwtSecret).parseClaimsJws(authToken);
+
+            return true;
+        } catch (final SignatureException e) {
+            log.error("Invalid JWT signature: {}", e.getMessage());
+        } catch (final MalformedJwtException e) {
+            log.error("Invalid JWT token: {}", e.getMessage());
+        } catch (final ExpiredJwtException e) {
+            log.error("JWT token is expired: {}", e.getMessage());
+        } catch(final UnsupportedJwtException e) {
+            log.error("JWT token is unsupported: {}", e.getMessage());
+        } catch (final IllegalArgumentException e) {
+            log.error("JWT claims string is empty: {}", e.getMessage());
+        }
+
+        return false;
     }
 
 }
