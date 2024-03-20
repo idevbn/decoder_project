c63ef014e517909f135fca07b2742a92d93e84c2
Idevaldo Neto <inetomec@yahoo.com.br>
2024-03-20 12:52:56 -0300

    feat: Adicionado pré autorização e lógica de verificação de permissão ao endpoint que salva uma inscrição

--- authuser/src/main/java/com/ead/authuser/controllers/InstructorController.java
+++ authuser/src/main/java/com/ead/authuser/controllers/InstructorController.java
@@ -1,13 +1,17 @@
 package com.ead.authuser.controllers;
 
 import com.ead.authuser.dtos.InstructorDTO;
+import com.ead.authuser.enums.RoleType;
 import com.ead.authuser.enums.UserType;
+import com.ead.authuser.models.RoleModel;
 import com.ead.authuser.models.UserModel;
+import com.ead.authuser.services.RoleService;
 import com.ead.authuser.services.UserService;
 import lombok.extern.log4j.Log4j2;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.*;
 
 import javax.validation.Valid;
@@ -22,12 +26,15 @@ import java.util.Optional;
 public class InstructorController {
 
     private final UserService userService;
+    private final RoleService roleService;
 
     @Autowired
-    public InstructorController(final UserService userService) {
+    public InstructorController(final UserService userService, final RoleService roleService) {
         this.userService = userService;
+        this.roleService = roleService;
     }
 
+    @PreAuthorize("hasAnyRole('ADMIN')")
     @PostMapping(value = "/subscription")
     public ResponseEntity<Object> saveSubscriptionInstructor(
             @RequestBody @Valid
@@ -40,9 +47,13 @@ public class InstructorController {
             return ResponseEntity.status(HttpStatus.NOT_FOUND).body("User not found.");
         }
 
+        final RoleModel roleModel = this.roleService.findByRoleName(RoleType.ROLE_INSTRUCTOR)
+                .orElseThrow(() -> new RuntimeException("Error: Role is Not Found."));
+
         final UserModel userModel = optionalUserModel.get();
         userModel.setUserType(UserType.INSTRUCTOR);
         userModel.setLastUpdateDate(LocalDateTime.now(ZoneId.of("UTC")));
+        userModel.getRoles().add(roleModel);
 
         this.userService.updateUser(userModel);
 
